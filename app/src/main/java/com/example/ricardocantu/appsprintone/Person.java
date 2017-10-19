e47c1d85c8bfab072ea018c8021810ed45bb8151
rickcantu20 <32943640+rickcantu20@users.noreply.github.com>
2017-10-19 17:33:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ricardocantu/appsprintone/Person.java
@@ -0,0 +1,17 @@
+package com.example.ricardocantu.appsprintone;
+
+/**
+ * Created by ricardo.cantu on 10/10/17.
+ */
+
+public class Person {
+    String name;
+    String age;
+    int photoID;
+
+    Person(String name, String age, int photoID){
+        this.name = name;
+        this.age = age;
+        this.photoID = photoID;
+    }
+}
